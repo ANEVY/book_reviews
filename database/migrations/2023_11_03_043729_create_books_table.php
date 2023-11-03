cd00d75c769b7354a72424cc0c2bec8463f36396
Akombo Neville Akwo <akomboneville55@gmail.com>
2023-11-03 06:30:31 +0100

    fake data for books and reviews

--- database/migrations/2023_11_03_043729_create_books_table.php
+++ database/migrations/2023_11_03_043729_create_books_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('books', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('auhor');
+            $table->string('author');
             $table->timestamps();
         });
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Book;
+use App\Models\Review;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,12 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
+
+        Book::factory(50)->create()->each(function($book){
+            $numReviews = random_int(5,20);
+            Review::factory(50)->count($numReviews)->for($book)->create();
+
+        });
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
