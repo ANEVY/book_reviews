c35f4d288e1cf1eea47bfcd4b495f5d9762f9bb4
Akombo Neville Akwo <akomboneville55@gmail.com>
2023-11-11 15:20:41 +0100

    Created book and review factory,created book resource controller and books view to display all books

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,6 +2,9 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Query\Builder as QueryBuilder;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -12,4 +15,29 @@ class Book extends Model
     public function reviews(){
         return $this->hasMany(Review::class);
     }
-}
+    public function scopeTitle(Builder $query, string $title){   
+        return $query->where("title","like","%".$title."%");
+    }
+    public function scopePopular(Builder $query,$from = null , $to = null){
+        return $query->withCount(["reviews"=>function(Builder $q) use($from ,$to)  {
+            if ($from && !$to) {
+                $q->where("created_at",">=",$from);
+            }else if( !$from && $to) {
+                $q->where("created_at","<=",$to);
+            }else if( $from && $to) {
+                $q->whereBetween("created_at", [$from,$to]);
+                // $q->where("created_at",">=",$from);
+                // $q->where("created_at","<=",$to);
+            }
+            
+            
+        }
+            
+        ])
+        ->orderBy("reviews_count","desc");
+    }
+    public function scopeHighestRated(Builder $query){
+        return $query->withAvg("reviews","rating")
+        ->orderBy("reviews_avg_rating","desc");
+    }
+}           
